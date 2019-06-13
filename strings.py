8153ce88ccb35066e96f2537cf89763d2d255416
abedigram <mbapost@yahoo.com>
2019-06-13 18:40:54 +0430

    strings

---
+++ strings.py
@@ -0,0 +1,30 @@
+apastrophy = "python's course for beginners"
+double_quote = 'pythons course for "beginners"'
+email = '''
+Hi dear!
+
+my name is Mohammad Bagher
+
+nice to meet u at "cafe'"
+
+yours
+
+'''
+# print(apastrophy)
+# print(double_quote)
+# print(email)
+
+course = 'python for beginners'
+print(course[0]) #p
+print(course[-2]) #r
+print(course[0:5]) # [0-3)
+print(course[2:]) # 2 to end // all minus "py" "01"
+print(course[:5]) # 0 to 4
+print(course[:]) # whole
+
+
+another = course[4:17]
+print(another)
+
+test = 'peugeot'
+print(test[1:-1])
