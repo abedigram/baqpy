40793ba3a1a3f6fbbd142cf8b6db143107148115
abedigram <mbapost@yahoo.com>
2019-06-13 18:15:45 +0430

    typecast

---
+++ typecast.py
@@ -0,0 +1,24 @@
+# birth_year = input('birth year:')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+weight = input('enter ur weight in Kg')
+pound = float(weight) * 2.20462
+print(pound)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
