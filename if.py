5273a5366b81fbe271bb3f6768d5095f1a4b2021
abedigram <mbapost@yahoo.com>
2019-06-22 19:38:36 +0430

    if

---
+++ if.py
@@ -0,0 +1,29 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print("it is a hot day")
+    print("drink water")
+elif is_cold:
+    print("it is a cold day")
+else:
+    print("enjou ur day")
+
+if is_cold and is_hot:
+    print("it's a dizzy day")
+
+if not is_cold and is_hot:
+    print("it's a HOT day")
+
+if is_cold or is_hot:
+    print("it's a day")
+
+temp = 30
+
+if temp > 30: # == != >=
+    print("hot")
+else:
+    print("not hot")
+
+#loop
+
