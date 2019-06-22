e545b01c68c5eb79ea2db1023798becd254983bc
abedigram <mbapost@yahoo.com>
2019-06-22 19:09:52 +0430

    arithamtics

---
+++ arith.py
@@ -0,0 +1,34 @@
+import math
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3) #float res
+print(10 // 3) #int res
+print(10 % 3)
+print(10 ** 3) # 1000
+
+x = 10
+x = x + 3
+x //= 3
+
+print(x)
+
+# precedense
+
+x = 10 ** 2 * (3 + 1)
+
+print(x)
+
+#functions
+
+x = 2.9
+print(round(x))
+
+print(math.ceil(x))
+print(math.floor(x))
+
+x = -2
+
+print(abs(x))
+
