31ff9967baa0899747036ef83542ae429c7e4474
abedigram <mbapost@yahoo.com>
2019-06-22 22:19:06 +0430

    list

---
+++ list.py
@@ -0,0 +1,47 @@
+names = ['john', 'bob', 'sarah', 'mary']
+
+print(names)
+print(names[0])
+print(names[2])
+print(names[1:3])
+
+names[0] = 'jon'
+print(names)
+
+
+matrix = [
+    [1,2,3],
+    [5,6,7],
+    [8,9,0]
+]
+print(matrix[0][1])
+
+for row in matrix:
+    for col in row:
+        print()# print(col)
+
+list = [1,2,3,4,5,6]
+
+list.append(7)
+
+list.insert(1,1.5)
+
+list.remove(1.5)
+
+# list.clear()
+
+list.pop()
+
+print(list.index(5))
+print(5 in list)
+
+list.append(5)
+print(list.count(5))
+
+list.sort()
+list.reverse()
+print(list)
+
+listt = list.copy()
+#listt = list -----points
+print(listt)
